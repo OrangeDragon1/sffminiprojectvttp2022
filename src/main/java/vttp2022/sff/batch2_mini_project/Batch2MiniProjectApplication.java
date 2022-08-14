cfa5a7f6bc398487288e187b070b45943911d8b2
OrangeDragon1 <wcks095@gmail.com>
2022-08-14 19:40:34 +0700

    initial commit

---
+++ src/main/java/vttp2022/sff/batch2_mini_project/Batch2MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.sff.batch2_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch2MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch2MiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/sff/batch2_mini_project/Batch2MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.sff.batch2_mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch2MiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
